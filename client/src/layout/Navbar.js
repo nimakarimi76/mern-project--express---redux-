3d03e1de54292c48bcd7725492c4f379e4040f29
nimakarimi76 <nimakarimi676@gmail.com>
2022-11-03 12:57:30 +0100

    fix navbar mobile view

--- client/src/layout/Navbar.js
+++ client/src/layout/Navbar.js
@@ -176,20 +176,30 @@ export default function Navbar() {
           <Disclosure.Panel className="sm:hidden">
             <div className="space-y-1 px-2 pt-2 pb-3">
               {navigation.map((item) => (
-                <Disclosure.Button
-                  key={item.name}
-                  as="a"
-                  href={item.href}
-                  className={classNames(
-                    item.current
-                      ? "bg-gray-900 text-white"
-                      : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                    "block px-3 py-2 rounded-md text-base font-medium"
-                  )}
-                  aria-current={item.current ? "page" : undefined}
+                <NavLink
+                  className={({ isActive }) =>
+                    isActive
+                      ? "bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"
+                      : "text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  }
+                  to={item.href}
                 >
                   {item.name}
-                </Disclosure.Button>
+                </NavLink>
+                // <Disclosure.Button
+                //   key={item.name}
+                //   as="a"
+                //   href={item.href}
+                //   className={classNames(
+                //     item.current
+                //       ? "bg-gray-900 text-white"
+                //       : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                //     "blfock px-3 py-2 rounded-md text-base font-medium"
+                //   )}
+                //   aria-current={item.current ? "page" : undefined}
+                // >
+                //   {item.name}
+                // </Disclosure.Button>
               ))}
             </div>
           </Disclosure.Panel>
